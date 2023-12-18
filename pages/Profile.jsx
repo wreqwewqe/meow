b25bc3417cb74f4b48a41746d8575a0d376619fd
wreqwewqe <1984850802@qq.com>
2023-12-18 12:12:46 +0800

    pro

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -63,38 +63,40 @@ export default function Profile() {
     return (
         <div className='h-screen'>
             <Header></Header>
-            <div className='box-border px-[11.2rem] py-[6.4rem]'>
+            <div className='box-border px-[11.2rem] py-[64px]'>
                 {/* <div className='w-[80px] h-[80px] bg-[yellow] rounded-[50%] mb-[16px]'></div> */}
                 <Blockies className='w-[8rem] h-[8rem] bg-[yellow] rounded-[50%] mb-[1.6rem]' seed={address ? address.toLowerCase() : ""} size={24} scale={4} />
-                <div suppressHydrationWarning className='text-[3.2rem] font-bold mb-[1.6rem]'>{address && (address.slice(0, 6) + "..." + address.slice(-4))}</div>
-                <div className='mb-[6.4rem]'><Image src={copy} onClick={copy_address} className='cursor-pointer mr-[8px]' height={15}></Image><span className='text-[1.6rem] text-[#5F6D7E]'>Copy address to clipboard</span></div>
-                <div className='flex justify-between mb-[3.2rem]'>
-                    <div className='px-[3.2rem] py-[2.4rem] border-solid border border-[#EAEBF0] rounded-[0.5rem] w-[46%] box-border'>
-                        <div className='mb-[2.4rem] flex items-center text-[1.6rem] text-[#272D37] font-bold'>$ MEO <Popover content={content1}><Image src={exclaim_point} className='cursor-pointer ml-[0.6rem]' height={15} /></Popover></div>
-                        <div className='text-[2.2rem] font-bold'>{loading ? "" : data.integral}</div>
+                <div suppressHydrationWarning className='min-[1px]:text-[24px] md:text-[3.2rem] font-bold mb-[1.6rem]'>{address && (address.slice(0, 6) + "..." + address.slice(-4))}</div>
+                <div className='mb-[64px]'><Image src={copy} onClick={copy_address} className='cursor-pointer mr-[8px]' height={15}></Image><span className='text-[16px]  text-[#5F6D7E]'>Copy address to clipboard</span></div>
+                <div className='md:flex md:justify-between mb-[32px]'>
+                    <div className='px-[32px] py-[24px] border-solid border border-[#EAEBF0] rounded-[5px] mb-[32px] md:mb-[0px] md:w-[46%] box-border'>
+                        <div className='mb-[24px] flex items-center text-[16px] text-[#272D37] font-bold'>$ MEO <Popover content={content1}><Image src={exclaim_point} className='cursor-pointer ml-[0.6rem]' height={15} /></Popover></div>
+                        <div className='text-[22px] font-bold'>{loading ? "" : data.integral}</div>
                     </div>
-                    <div className='px-[3.2rem] py-[2.4rem] border-solid border border-[#EAEBF0] rounded-[0.5rem] w-[46%] box-border'>
-                        <div className='mb-[2.4rem] flex items-center text-[1.6rem] text-[#272D37] font-bold'>$ Meow <Popover content={content2}><Image src={exclaim_point} className='cursor-pointer ml-[0.6rem]' height={15} /></Popover></div>
-                        <div className='text-[2.2rem] font-bold'>{loading ? "" : data.token}</div>
+                    <div className='px-[32px] py-[24px] border-solid border border-[#EAEBF0] rounded-[5px] md:w-[46%] box-border'>
+                        <div className='mb-[24px] flex items-center text-[16px] text-[#272D37] font-bold'>$ Meow <Popover content={content2}><Image src={exclaim_point} className='cursor-pointer ml-[0.6rem]' height={15} /></Popover></div>
+                        <div className='text-[22px] font-bold'>{loading ? "" : data.token}</div>
                     </div >
                 </div>
-                <div className='box-border border-solid border p-[3.2rem] border-[#EAEBF0] rounded-[0.5rem] mb-[3.2rem]'>
-                    <div className='text-[2.4rem] font-bold mb-[4rem]'>Invite My Friends <Popover content={content3}><Image src={exclaim_point} className='cursor-pointer' height={16} /></Popover></div>
-                    <div className='flex mb-[2.4rem]'><Input className='mr-[2.3rem]' value={loading ? "" : data.inviteCode} readOnly></Input> <div className='box-border bg-[#F4B512] px-[2rem] py-[1.2rem] w-[11.9rem] h-[4.8rem] rounded-[0.6rem] text-[white] font-semibold cursor-pointer text-[1.4rem] border-none' onClick={copy_link}>Copy Link</div></div>
-                    <div className='text-[1.6rem] text-[#5F6D7E]'>Copy the link above to start inviting friends</div>
+                <div className='box-border border-solid border p-[32px] border-[#EAEBF0] rounded-[5px] mb-[32px]'>
+                    <div className='text-[24px] font-bold mb-[12px] '>Invite My Friends <Popover content={content3}><Image src={exclaim_point} className='cursor-pointer' height={16} /></Popover></div>
+                    <div className='mb-[32px] md:flex md:mb-[2.4rem]'><Input className='md:mr-[2.3rem] mb-[26px] md:mb-[0px]' value={loading ? "" : data.inviteCode} readOnly></Input> <span className='md:box-border bg-[#F4B512] px-[20px] py-[12px] md:w-[11.9rem] min-[1px]:inline  rounded-[6px] text-[white] font-semibold cursor-pointer  text-[16px] border-none' onClick={copy_link}>Copy Link</span></div>
+                    <div className='text-[16px] text-[#5F6D7E]'>Copy the link above to start inviting friends</div>
                 </div>
 
-                <div className='p-[3.2rem] h-[71rem] border-solid rounded-[0.5rem]  border-[#EAEBF0] mb-[1.2rem]' >
-                    <div className='flex justify-between items-end mb-[1.6rem]'>
-                        <div className='text-[2.4rem] font-bold'>Monthly $Meow Earning Ranking</div>
-                        <div className='text-[1.6rem] font-bold'>My Monthly $Meow Earning: <span className='text-[#F4B512]'>{loading ? "" : data.monthIntegral}</span></div>
+                <div className='p-[32px] md:h-[71rem] border-solid rounded-[0.5rem]  border-[#EAEBF0] mb-[1.2rem]' >
+                    <div className='md:flex md:justify-between md:items-end mb-[16px]'>
+                        <div className=' text-[24px]   font-bold mb-[10px] mdLmb-[10px]'>Monthly $Meow Earning Ranking</div>
+                        <div className='text-[16px] font-bold'>My Monthly $Meow Earning: <span className='text-[#F4B512]'>{loading ? "" : data.monthIntegral}</span></div>
                     </div>
-                    <div className='flex justify-between text-[#5F6D7E] text-[1.6rem]  h-[4.4rem]  border-solid border-x-0 border-t-0  border-b-1 border-[#EAEBF0] '>
+                    <div className='hidden md:flex justify-between text-[#5F6D7E] text-[1.6rem]  h-[4.4rem]  border-solid border-x-0 border-t-0  border-b-1 border-[#EAEBF0] '>
                         <div className='basis-[13%]'>Number</div>
                         <div className='basis-[80%] whitespace-nowrap '>Wallet Address</div>
                         <div className='basis-[6.7%]'>Quantity</div>
                     </div>
-                    <div className='overflow-y-auto h-[55.2rem] no_scroll'>
+
+
+                    <div className='hidden md:block overflow-y-auto h-[55.2rem] no_scroll'>
                         {loading ? "" : data.top50.map((item, index) => (
                             <div className={' flex justify-between items-center h-[7.2rem] box-border   border-solid border-x-0 border-t-0  border-b-1 border-[#EAEBF0] text-[1.6rem] font-bold ' + (item.Address.toLowerCase() == address.toLowerCase() ? "bg-[#F4B512]/[0.3]" : "")}>
                                 <div className='basis-[13%] text-[#3d331b]'>{index + 1}</div>
@@ -106,8 +108,32 @@ export default function Profile() {
                             </div>
                         ))}
                     </div>
+                    <div className='md:hidden  h-[600px] overflow-auto '>
+                        {loading ? "" : data.top50.map((item, index) => (
+                            <div className='py-[16px]  border-solid boder-1 border-[white] border-t-[#EAEBF0]'>
+                                <div className='flex'>
+                                    <div className='w-[65px]'>
+                                        <div className='text-[12px] text-[#5F6D7E]'>{index + 1}</div>
+                                        <div className='text-[16px] text-[#F4B512]'>1</div>
+                                    </div>
+                                    <div>
+                                        <div className='text-[12px] text-[#5F6D7E]'>Wallet Address</div>
+                                        <div className='text-[16px] text-[#F4B512] flex items-center'>
+                                            <div className='w-[48px] h-[48px] bg-[#F4B512] mr-[12px] rounded-[50%]' ></div>
+                                            <div className='text-[16px] font-bold'>{item.Address.slice(0, 6) + "..." + item.Address.slice(-4)}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className='text-[12px] text-[#5F6D7E] mt-[16px]'>Quantity</div>
+                                    <div className='text-[16px] font-bold'>{item.Quantity}</div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
 
-                    <div className='text-[1.6rem] text-[#5F6D7E]'>Display up to 50 lines</div>
+
+                    <div className='hidden md:block text-[1.6rem] text-[#5F6D7E]'>Display up to 50 lines</div>
                 </div>
             </div>
             <Footer></Footer>
